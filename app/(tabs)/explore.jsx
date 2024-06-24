db0ba3f380a0f65956e9d7a49a15a8a378fc4aa2
Tanmay Patil <patil.tanmay0697@gmail.com>
2024-06-24 16:03:42 -0700

    Add a button for add to library functionality. Wrap long names

--- app/(tabs)/explore.jsx
+++ app/(tabs)/explore.jsx
@@ -1,10 +1,11 @@
-import {FlatList, Text, View} from 'react-native'
+import {FlatList, Text, TouchableOpacity, View} from 'react-native'
 import React, {useEffect, useState} from 'react'
 import {SafeAreaView} from "react-native-safe-area-context";
 import {images} from "../constants";
 import {Image} from "react-native";
 import SearchInput from "../components/SearchInput";
 import {SPOTIFY_SEARCH_DATA} from "../constants/spotify_search_data";
+import {icons} from "../constants";
 
 const CLIENT_ID = process.env["CLIENT_ID"];
 const CLIENT_SECRET = process.env["CLIENT_SECRET"];
@@ -84,19 +85,29 @@ const Explore = () => {
     }
 
     return (
-        <SafeAreaView className="bg-primary h-full ">
-            <View className="ml-4">
+        <SafeAreaView className="bg-primary w-full h-full ">
+            <View className="mx-2">
             <FlatList data={SPOTIFY_SEARCH_DATA.tracks.items} keyExtractor={(item) => item.id.toString()}
                       renderItem={({item}) => (
-                          <View className="flex-row py-[2px]">
-                              <Image
-                                  source={{uri: item.album.images[0].url}}
-                                  className="p-12"
-                                  resizeMode="contain"></Image>
-                              <View className="flex-col justify-center">
-                                  <Text className="font-psemibold text-white px-8">{item.name}</Text>
-                                  <Text className="font-pregular text-white px-8">{getArtistNames(item.artists)}</Text>
+                          <View className="flex-row py-[2px] justify-between">
+                              <View className="flex-row flex-1">
+                                  <Image
+                                      source={{uri: item.album.images[0].url}}
+                                      className="p-12"
+                                      resizeMode="contain"></Image>
+                                  <View className="flex-col justify-center">
+                                      <Text numberOfLines={1} className="w-48 font-psemibold text-white px-4">{item.name}</Text>
+                                      <Text numberOfLines={1} className="w-48 font-pregular text-white px-4">{getArtistNames(item.artists)}</Text>
+                                  </View>
+
+
                               </View>
+                              <TouchableOpacity className="justify-center">
+                                  <Image source={icons.plus} className="w-8 h-8 p-2 mx-8" resizeMode="contain"></Image>
+                              </TouchableOpacity>
+
+
+
 
                           </View>
 
