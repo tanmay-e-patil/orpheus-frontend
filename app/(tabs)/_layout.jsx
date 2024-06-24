5f6d1abf1e13b2872f2877ac7d3d13764a1f15fe
Tanmay Patil <patil.tanmay0697@gmail.com>
2024-06-24 16:00:58 -0700

    Fix status bar for dark theme

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -2,6 +2,7 @@ import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { Tabs, Redirect } from 'expo-router'
 import { icons } from '../constants'
+import {StatusBar} from "expo-status-bar";
 
 const TabIcon = ({ icon, color, name, focused }) => {
     return (
@@ -85,6 +86,8 @@ const TabsLayout = () => {
                     }}
                 />
             </Tabs>
+            <StatusBar backgroundColor='#161622' style='light'></StatusBar>
+
         </>
     )
 }
