d74779d325719fa4dbf470bdf095f342c61391d4
Tanmay Patil <patil.tanmay0697@gmail.com>
2024-06-25 15:17:21 -0700

    Temporary changes for library tab

--- app/(tabs)/library.jsx
+++ app/(tabs)/library.jsx
@@ -1,39 +1,67 @@
-import {View, Text, FlatList, Image} from 'react-native'
+import {View, Text, FlatList, Image, TouchableOpacity} from 'react-native'
 import React from 'react'
 import {SafeAreaView} from "react-native-safe-area-context";
-import {images} from "../constants";
+import {icons, images} from "../constants";
 import SearchInput from "../components/SearchInput";
+import {SPOTIFY_SEARCH_DATA} from "../constants/dummy_data/spotify_search_data";
 
 const Library = () => {
     return (
-        <SafeAreaView className="bg-primary h-full">
-            <FlatList data={[{id: 1}, {id: 2},]} keyExtractor={(item) => item.id.toString()} renderItem={({item}) => (
-                <Text className="text-3xl text-white">{item.id}</Text>
-            )}
-                      ListHeaderComponent={() => (
-                          <View className="my-6 px-4 space-y-6">
-                              <View className="justify-between items-start flex-row mb-6">
-                                  <View>
-                                      <Text className="text-sm text-gray-100 font-pmedium">
-                                          Welcome
-                                      </Text>
-                                      <Text className="text-2xl text-gray-100 font-psemibold">
-                                          JSMastery
-                                      </Text>
+        <SafeAreaView className="bg-primary w-full h-full ">
+            <View className="mx-2">
+                <FlatList data={SPOTIFY_SEARCH_DATA.tracks.items} keyExtractor={(item) => item.id.toString()}
+                          renderItem={({item}) => (
+                              <View className="flex-row py-[2px] justify-between">
+                                  <View className="flex-row flex-1">
+                                      <Image
+                                          source={{uri: item.album.images[0].url}}
+                                          className="p-12"
+                                          resizeMode="contain"></Image>
+                                      <View className="flex-col justify-center">
+                                          <Text numberOfLines={1} className="w-48 font-psemibold text-white px-4">{item.name}</Text>
+                                          {/*<Text numberOfLines={1} className="w-48 font-pregular text-white px-4">{getArtistNames(item.artists)}</Text>*/}
+                                      </View>
+
 
                                   </View>
-                                  <View>
-                                      <Image source={images.logoSmall} className="w-9 h-10" resizeMode="contain"></Image>
-                                  </View>
+                                  <TouchableOpacity className="justify-center">
+                                      <Image source={icons.plus} className="w-12 h-12 p-2 mx-8" resizeMode="contain"></Image>
+                                  </TouchableOpacity>
+
+
+
 
                               </View>
-                              <SearchInput placeholder="Search for songs"/>
-                              <View className="w-full flex-1 "></View>
-                          </View>
-                      )}
-            >
 
-            </FlatList>
+                          )}
+                          ListHeaderComponent={() => (
+                              <View className="my-6 pr-4 space-y-6">
+                                  <View className="justify-between items-start flex-row w-full">
+
+                                      <View>
+                                          <Image source={images.logoSmall} className="w-9 h-10 mr-4 my-2"
+                                                 resizeMode="contain"></Image>
+                                      </View>
+                                      <View className="flex-1">
+                                          <SearchInput placeholder="Search for new songs, artists, albums" onSearch={(event) => {
+
+                                              console.log("searching", event.nativeEvent.text);
+                                              setQuery(event.nativeEvent.text);
+                                          }} className="p-8"/>
+
+                                      </View>
+
+
+                                  </View>
+
+                                  <View className="w-full flex-1 "></View>
+                              </View>
+                          )}
+                >
+
+                </FlatList>
+            </View>
+
         </SafeAreaView>
     )
 }
