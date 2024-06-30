6e11793acd535fb40f61028daacc99d2ea92ce9e
Tanmay Patil <patil.tanmay0697@gmail.com>
2024-06-30 10:18:33 -0700

    Add more song metadata on library screen

--- app/(tabs)/library.jsx
+++ app/(tabs)/library.jsx
@@ -1,94 +1,98 @@
-import {Alert, FlatList, Image, RefreshControl, Text, View} from 'react-native'
-import React, {useEffect, useState} from 'react'
-import {SafeAreaView} from "react-native-safe-area-context";
-import axios from "axios";
-import {SONGS_API_URL} from "../constants/strings";
-import EmptyState from "../components/EmptyState";
-import SearchInput from "../components/SearchInput";
-import {images} from "../constants";
+import { Alert, FlatList, Image, RefreshControl, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import axios from 'axios';
+import { SONGS_API_URL } from '../constants/strings';
+import EmptyState from '../components/EmptyState';
+import SearchInput from '../components/SearchInput';
+import { images } from '../constants';
 // import {images} from "../constants/images"
 
 const Library = () => {
-    const [library, setLibrary] = useState([])
-    const loadLibrary = async () => {
-        try {
-            const result = await axios.get(SONGS_API_URL)
-            setLibrary(result.data)
-            console.log(result.data)
+  const [library, setLibrary] = useState([]);
+  const loadLibrary = async () => {
+    try {
+      const result = await axios.get(SONGS_API_URL);
+      setLibrary(result.data);
+      console.log(result.data);
 
-        } catch (e) {
-            Alert.alert("Failed to fetch songs to library", e)
-        }
+    } catch (e) {
+      Alert.alert('Failed to fetch songs to library', e);
     }
+  };
 
-    const [refreshing, setRefreshing] = useState(false);
-    const onRefresh = async () => {
-        setRefreshing(true);
-        await loadLibrary();
-        setRefreshing(false);
-    };
+  const [refreshing, setRefreshing] = useState(false);
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await loadLibrary();
+    setRefreshing(false);
+  };
 
-    useEffect(() => {
-        loadLibrary().then(r => console.log("Loaded library"))
-    }, []);
+  useEffect(() => {
+    loadLibrary().then(r => console.log('Loaded library'));
+  }, []);
 
-    return (
-        <SafeAreaView className="bg-primary w-full h-full ">
-            <View className="mx-2">
-                <FlatList data={library} keyExtractor={(item) => item.ID}
-                          renderItem={({item}) => (
-                              <View className="flex-row py-[2px] justify-between">
-                                  <View className="flex-row flex-1">
-                                      <Image
-                                          source={{uri: item.AlbumArt}}
-                                          className="p-12"
-                                          resizeMode="contain"></Image>
-                                      <View className="flex-col justify-center">
-                                          <Text numberOfLines={1}
-                                                className="w-48 font-psemibold text-white px-4">{item.Name}</Text>
-                                      </View>
-
-
-                                  </View>
-
-
-                              </View>
-
-                          )}
-                          ListHeaderComponent={() => (
-                              <View className="my-6 pr-4 space-y-6">
-                                  <View className="justify-between items-start flex-row w-full">
-
-                                      <View>
-                                          <Image source={images.logoSmall} className="w-9 h-10 mr-4 my-2"
-                                                 resizeMode="contain"></Image>
-                                      </View>
-                                      <View className="flex-1">
-                                          <SearchInput placeholder="Search for new songs, artists, albums"
-                                                       onSearch={(event) => {
-
-                                                           console.log("searching", event.nativeEvent.text);
-                                                           setQuery(event.nativeEvent.text);
-                                                       }} className="p-8"/>
-                                      </View>
+  return (
+    <SafeAreaView className="bg-primary w-full h-full ">
+      <View className="mx-2 h-full">
+        <FlatList data={library} keyExtractor={(item) => item.ID}
+                  renderItem={({ item }) => (
+                    <View className="flex-row py-[2px] justify-between">
+                      <View className="flex-row flex-1">
+                        <Image
+                          source={{ uri: item.AlbumArt }}
+                          className="p-12"
+                          resizeMode="contain"></Image>
+                        <View className="flex-col justify-center">
+                          <Text numberOfLines={1}
+                                className="w-48 font-psemibold text-lg text-white px-4">{item.Name}</Text>
+                          <Text numberOfLines={1}
+                                className="w-48 font-pregular text-sm text-gray-100 px-4">{item.ArtistName}</Text>
+                          <Text numberOfLines={1}
+                                className="w-48 font-pregular text-sm text-gray-100 px-4">{item.Duration}</Text>
+                        </View>
 
 
-                                  </View>
-
-                                  <View className="w-full flex-1 "></View>
-                              </View>
-                          )} ListEmptyComponent={() => (<EmptyState title="No Songs Found"
-                                                                    subtitle="Add music via the explore tab."></EmptyState>)}
-                          refreshControl={<RefreshControl refreshing={refreshing}
-                                                          onRefresh={onRefresh}></RefreshControl>}>
-
-                </FlatList>
-
-            </View>
+                      </View>
 
 
-        </SafeAreaView>
-    )
-}
+                    </View>
 
-export default Library
+                  )}
+                  ListHeaderComponent={() => (
+                    <View className="my-6 pr-4 space-y-6">
+                      <View className="justify-between items-start flex-row w-full">
+
+                        <View>
+                          <Image source={images.logoSmall} className="w-9 h-10 mr-4 my-2"
+                                 resizeMode="contain"></Image>
+                        </View>
+                        <View className="flex-1">
+                          <SearchInput placeholder="Search for new songs, artists, albums"
+                                       onSearch={(event) => {
+
+                                         console.log('searching', event.nativeEvent.text);
+                                         setQuery(event.nativeEvent.text);
+                                       }} className="p-8" />
+                        </View>
+
+
+                      </View>
+
+                      <View className="w-full flex-1 "></View>
+                    </View>
+                  )} ListEmptyComponent={() => (<EmptyState title="No Songs Found"
+                                                            subtitle="Add music via the explore tab."></EmptyState>)}
+                  refreshControl={<RefreshControl refreshing={refreshing}
+                                                  onRefresh={onRefresh}></RefreshControl>}>
+
+        </FlatList>
+
+      </View>
+
+
+    </SafeAreaView>
+  );
+};
+
+export default Library;
---
+++ app/context/AuthContext.jsx
@@ -0,0 +1,158 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import { ACCESS_TOKEN_KEY, LOGIN_API_URL, ME_API_URL, SIGN_UP_API_URL } from '../constants/strings';
+import * as SecureStore from 'expo-secure-store';
+import { router } from 'expo-router';
+
+export const AuthContext = createContext({});
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+
+export const AuthProvider = ({ children }) => {
+
+  const [authState, setAuthState] = useState({
+    token: null, authenticated: false,
+  });
+
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    userId: '',
+  });
+
+  useEffect(() => {
+    const loadToken = async () => {
+      const token = await SecureStore.getItemAsync(ACCESS_TOKEN_KEY);
+      console.log('stored: ', token);
+      return token;
+    };
+
+    const getUser = async () => {
+      try {
+        const token = await loadToken();
+        if (token) {
+          axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        } else {
+          console.log('Logging out as token not found');
+          await logout();
+          return;
+        }
+        console.log(axios.defaults.headers.common['Authorization']);
+        const response = await axios.get(ME_API_URL, {
+          withCredentials: true,
+        });
+
+        setAuthState({
+          token: token,
+          authenticated: true,
+        });
+
+        setUser({
+          username: response.data.username,
+          email: response.data.email,
+          userId: response.data.id,
+        });
+
+      } catch (e) {
+        await logout();
+      } finally {
+        console.log('Done fetching user');
+      }
+
+    };
+    getUser().then(r => console.log('Fetched user'));
+
+
+  }, []);
+
+  const register = async (email, username, password) => {
+    try {
+      const result = await axios.post(SIGN_UP_API_URL, {
+        email, username, password,
+      });
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true,
+      });
+
+      setUser({
+        username: username,
+        email: email,
+        userId: result.data.id,
+      });
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`;
+
+      await SecureStore.setItemAsync(ACCESS_TOKEN_KEY, result.data.access_token);
+      return result;
+    } catch (e) {
+      return { error: true, msg: e?.response.data.msg };
+    }
+  };
+
+  const login = async (email, password) => {
+    try {
+      const result = await axios.post(LOGIN_API_URL, {
+        email, password,
+      });
+
+      setAuthState({
+        token: result.data.access_token,
+        authenticated: true,
+      });
+
+      setUser({
+        username: result.data.username,
+        email: email,
+        userId: result.data.id,
+      });
+
+      axios.defaults.headers.common['Authorization'] = `Bearer ${result.data.access_token}`;
+
+      await SecureStore.setItemAsync(ACCESS_TOKEN_KEY, result.data.access_token);
+      return result;
+
+    } catch (e) {
+      return { error: true, msg: e?.response.data.msg };
+    }
+
+
+  };
+
+  const logout = async () => {
+    await SecureStore.deleteItemAsync(ACCESS_TOKEN_KEY);
+
+    axios.defaults.headers.common['Authorization'] = '';
+
+    setAuthState({
+      token: null,
+      authenticated: false,
+    });
+
+    setUser({
+      username: '',
+      email: '',
+      userId: '',
+    });
+
+    router.replace('/');
+  };
+
+  const value = {
+    onRegister: register,
+    onLogin: login,
+    onLogout: logout,
+    authState,
+    user,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
